94ee3277d13b0b2cc098c65d3b1c566cf1c401e1
Aniket Chaudhary <aniket.chaudhary13579@gmail.com>
2023-09-02 02:25:04 +0530

    Update active.js

--- active.js
+++ active.js
@@ -1,8 +1,8 @@
 var page;
-console.log(window.location.pathname.split(".")[0].slice(1));
 switch(window.location.pathname.split(".")[0].slice(1)){
     case "":
         page = "Home"; break;
     case "Blogs":
         page = "Blogs"; break;
 }
+console.log(page);
